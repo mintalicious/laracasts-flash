605f129d80e1949d44544ace77b06430e6670ff4
mintalicious <mintalicious@web4ward.de>
2024-05-02 11:17:02 +0200

    Update message.blade.php

--- src/views/message.blade.php
+++ src/views/message.blade.php
@@ -1,22 +1,29 @@
 @foreach (session('flash_notification', collect())->toArray() as $message)
     @if ($message['overlay'])
-        @include('flash::modal', [
-            'modalClass' => 'flash-modal',
-            'title'      => $message['title'],
-            'body'       => $message['message']
-        ])
+        @include('flash::overlay', ['message' => $message])
     @else
-        <div class="alert
-                    alert-{{ $message['level'] }}
-                    {{ $message['important'] ? 'alert-important' : '' }}"
-                    role="alert"
-        >
+        <div class="alert alert-{{ $message['level'] }} {{ $message['important'] ? 'alert-dismissible' : '' }}" role="alert">
+            @switch($message['level'])
+                @case('info')
+                    <i class="bi bi-info-square"></i>
+                    @break
+                @case('success')
+                    <i class="bi bi-check-circle"></i>
+                    @break
+                @case('warning')
+                    <i class="bi bi-exclamation-circle"></i>
+                    @break
+                @case('danger')
+                    <i class="bi bi-x-circle"></i>
+                    @break
+                @case('critical')
+                    <i class="bi bi-exclamation-triangle"></i>
+                    @break
+                @default
+
+            @endswitch
             @if ($message['important'])
-                <button type="button"
-                        class="close"
-                        data-dismiss="alert"
-                        aria-hidden="true"
-                >&times;</button>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             @endif
 
             {!! $message['message'] !!}
